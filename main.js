d05e431101fcc71a1405300c93d272037038b48b
Mieu Teacher <mieuteacher@gmail.com>
2023-09-16 20:09:51 +0700

    init

---
+++ main.js
@@ -0,0 +1,44 @@
+//let number = Number(prompt("nhập vào 1 số!"))
+
+// if(number % 2 == 0) {
+//     alert("Bạn vừa nhập số chẵn!")
+// }else {
+//     alert("Bạn vừa nhập số lẽ!")
+// }
+
+// if(number > 100) {
+//     alert("Bạn vừa nhập số lớn hơn 100!")
+// }else if(number > 10) {
+//     alert("Bạn vừa nhập số lớn hơn 10")
+// }else {
+//     alert("Bạn vừa nhập 1 số!")
+// }
+
+// switch (number) { 
+//     case (number > 5):
+//         alert("Vào case 1")
+//     case (number > 10):
+//         alert("Vào case 2")
+//     case (number > 15):
+//         console.log("đã vào!")
+//         alert("Vào case 3")
+//         break
+//     default:
+//         alert("Vào default!")
+// }
+
+// for, while, do while
+
+let numbers = [1,2,3,4,5];
+
+for (let i = 1; i < 5; i++) {
+    console.log("i", i)
+}
+
+console.log("i", i)
+// start
+// i = 1 => true => 1 => 1++
+// i = 2 => true => 2 => 2++
+// i = 3 => true => 3 => 3++
+// i = 4 => true => 4 => 4++
+// i = 5 => false => end
